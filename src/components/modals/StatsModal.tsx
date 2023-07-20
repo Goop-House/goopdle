7ea41dc56744180d465dffb03beccdbdf69468f4
TheLickIn13Keys <bardiaanvari10@gmail.com>
2023-07-20 16:22:11 -0700

    first commit

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -77,29 +77,29 @@ export const StatsModal = ({
       isOpen={isOpen}
       handleClose={handleClose}
     >
-      <StatBar gameStats={gameStats} />
-      <h4 className="text-lg font-medium leading-6 text-gray-900 dark:text-gray-100">
-        {GUESS_DISTRIBUTION_TEXT}
-      </h4>
-      <Histogram
+      {/* <StatBar gameStats={gameStats} /> */}
+      <h1 className="mt-5 columns-1 items-center items-stretch justify-center text-center dark:text-white sm:mt-10">
+        {'GOOP Day 7/21'}
+      </h1>
+      {/* <Histogram
         isLatestGame={isLatestGame}
         gameStats={gameStats}
         isGameWon={isGameWon}
         numberOfGuessesMade={numberOfGuessesMade}
-      />
+      /> */}
       {(isGameLost || isGameWon) && (
         <div className="mt-5 columns-2 items-center items-stretch justify-center text-center dark:text-white sm:mt-6">
           <div className="inline-block w-full text-left">
-            {(!ENABLE_ARCHIVED_GAMES || isLatestGame) && (
-              <div>
-                <h5>{NEW_WORD_TEXT}</h5>
-                <Countdown
-                  className="text-lg font-medium text-gray-900 dark:text-gray-100"
-                  date={tomorrow}
-                  daysInHours={true}
-                />
-              </div>
-            )}
+            {/* {(!ENABLE_ARCHIVED_GAMES || isLatestGame) && (
+              // <div>
+              //   <h5>{NEW_WORD_TEXT}</h5>
+              //   <Countdown
+              //     className="text-lg font-medium text-gray-900 dark:text-gray-100"
+              //     date={tomorrow}
+              //     daysInHours={true}
+              //   />
+              // </div>
+            )} */}
             {ENABLE_ARCHIVED_GAMES && !isLatestGame && (
               <div className="mt-2 inline-flex">
                 <ClockIcon className="mr-1 mt-2 mt-1 h-5 w-5 stroke-black dark:stroke-white" />
@@ -113,7 +113,7 @@ export const StatsModal = ({
               </div>
             )}
           </div>
-          <div>
+          {/* <div>
             <button
               type="button"
               className="mt-2 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-center text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-base"
@@ -133,13 +133,7 @@ export const StatsModal = ({
               <ShareIcon className="mr-2 h-6 w-6 cursor-pointer dark:stroke-white" />
               {SHARE_TEXT}
             </button>
-          </div>
-        </div>
-      )}
-      {ENABLE_MIGRATE_STATS && (
-        <div>
-          <hr className="mt-4 -mb-4 border-gray-500" />
-          <MigrationIntro handleMigrateStatsButton={handleMigrateStatsButton} />
+          </div> */}
         </div>
       )}
     </BaseModal>
